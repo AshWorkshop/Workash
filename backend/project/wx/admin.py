42d6ea62e075f84d79fe122164d53932c1d504d3
Nouzan <clobraindie@outlook.com>
2018-03-03 13:26:20 +0800

    change structure

--- backend/project/wx/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from project.wx.models import WxUser
-
-
-admin.site.register(WxUser)
--- backend/project/wx/utils/bases.py
+++
@@ -1,6 +0,0 @@
-from rest_framework.views import APIView
-
-
-class BaseView(APIView):
-    def get_session(self):
-        return self.request.auth
