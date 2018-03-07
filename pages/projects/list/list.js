140e5a19192c229837c5af0cf878e6efa6ebc6a1
Nouzan <clobraindie@outlook.com>
2018-03-07 14:06:30 +0800

    add part-list

--- pages/projects/list/list.js
+++ pages/projects/list/list.js
@@ -1,66 +1,71 @@
 // pages/projects/list/list.js
+var app = getApp()
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
+    /** 
+        * 页面配置 
+        */
+    winWidth: 0,
+    winHeight: 0,
+    // tab切换  
+    currentTab: 0,
+    parts: [],
   },
+  onLoad: function () {
+    var that = this;
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
+    /** 
+     * 获取系统信息 
+     */
+    wx.getSystemInfo({
+
+      success: function (res) {
+        that.setData({
+          winWidth: res.windowWidth,
+          winHeight: res.windowHeight
+        });
+      }
+
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
   onShow: function () {
-  
+    var that = this;
+    if (app.globalData.worker.isPropLoaded) {
+      that.getParts();
+    } else {
+      app.workerReadyCallback = () => {
+        that.getParts();
+      }
+    }
   },
+  /** 
+     * 滑动切换tab 
+     */
+  bindChange: function (e) {
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
+    var that = this;
+    that.setData({ currentTab: e.detail.current });
+
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
+  /** 
+   * 点击tab切换 
    */
-  onUnload: function () {
-  
+  swichNav: function (e) {
+
+    var that = this;
+
+    if (this.data.currentTab === e.target.dataset.current) {
+      return false;
+    } else {
+      that.setData({
+        currentTab: e.target.dataset.current
+      })
+    }
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  getParts: function () {
+    console.log(app.globalData.worker.participations)
+    this.setData({
+      parts: app.globalData.worker.participations
+    });
   }
 })
